c2363a8bb66bbc1249bf35718b1eaef279324463
claudio <claudio.sousa@epfl.ch>
2018-02-28 14:50:09 +0100

    [NRRPLT-6158] ROS terminal auto-completion and new commands (Bitbucket #41 and #43)
    Change-Id: I9017dcecf4d0e6da8a059396d0d2a32faa04e11d

--- src/rosbridge_suite-develop/rosbridge_server/scripts/ros_commander.py
+++ src/rosbridge_suite-develop/rosbridge_server/scripts/ros_commander.py
@@ -2,7 +2,7 @@
 Executes ros commands
 """
 
-import subprocess
+import subprocess, os
 from tornado.gen import Task, Return, coroutine
 import tornado.process
 from tornado.ioloop import IOLoop
@@ -10,12 +10,12 @@ import time
 
 
 class RosCommandExecuter():
-
     """
     Executes ros commands
     """
 
-    VALID_COMMANDS = {'rostopic', 'rosservice'}
+    VALID_COMMANDS = {'rostopic', 'rosservice', 'rosmsg', 'rossrv', 'rosnode', 'help'}
+    ROS_COMPLETER_SCRIPT = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'roscommand_completer.sh')
 
     def __init__(self, sendmsg):
         self.sendmsg = sendmsg
@@ -27,22 +27,44 @@ class RosCommandExecuter():
         handles incoming ros cmd message
         """
 
-        print 'RosCommandExecuter command received', msg
         cmd = msg['cmd']
-        if cmd == 'STOP_CURRENT_EXECUTION':
+        if cmd == 'COMPLETE':
+            self.complete_cmd(msg['args'])
+        elif cmd == 'STOP_CURRENT_EXECUTION':
             if self.current_process:
                 self.current_process.kill()
         elif cmd in self.VALID_COMMANDS:
-            args = msg['args']
-            self.execute(cmd, args)
+            self.execute(cmd, msg['args'])
         else:
-            self.sendRosResponse(
-                data=['Invalid command: ' + cmd], running=False)
-            print "Invalid command: ", cmd
+            self.sendRosResponse(data=[{'data': 'Invalid command: ' + cmd, 'type': 'stderr'}], running=False)
 
     def sendRosResponse(self, data=None, running=True):
-        self.sendmsg(
-            {'op': 'ros_response', 'msg': {'data': data}, 'running': running})
+        self.sendmsg({'op': 'ros_response', 'msg': {'data': data}, 'running': running})
+
+    def complete_cmd(self, cmd):
+        res = self.retrieve_completion(cmd)
+        if res:
+            self.sendRosResponse(data=res, running=False)
+
+    def retrieve_completion(self, cmd):
+        parts = [p for p in cmd.split(' ') if p]
+        if parts[0] == 'help':
+            return None
+
+        l = len(parts)
+        if cmd[-1] == ' ':
+            l += 1
+
+        if l <= 1:
+            completion = [c for c in self.VALID_COMMANDS if c.startswith(cmd)]
+            return [{'data': completion, 'type': 'completion'}]
+
+        if parts[0] not in self.VALID_COMMANDS:
+            return [{'data': 'Invalid command: ' + parts[0], 'type': 'stderr'}]
+
+        res = subprocess.check_output([self.ROS_COMPLETER_SCRIPT] + [cmd] + [str(max(1, l - 1))], shell=False)
+        res = [l for l in res.split('\n') if l]
+        return [{'data': res, 'type': 'completion'}]
 
     def finish(self):
         """
@@ -53,22 +75,16 @@ class RosCommandExecuter():
 
     def execute(self, cmd, args):
         try:
-            self.current_process = subprocess.Popen(
-                [cmd] + args,
-                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            self.current_process = subprocess.Popen([cmd] + args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
             # we must buffer otherwise too frequent messages will overload the client
-            buffering = {
-                'data': [],
-                'start_time': time.time()
-            }
+            buffering = {'data': [], 'start_time': time.time()}
 
             def stdout_data_available(fd, mode):
                 try:
                     if mode == 16:  # stream closed
                         IOLoop.current().remove_handler(fd)
-                        self.sendRosResponse(
-                            data=buffering['data'], running=False)
+                        self.sendRosResponse(data=buffering['data'], running=False)
 
                         if self.current_process:  # could have been killed by STOP_CURRENT_EXECUTION
                             self.current_process.stdout.close()
@@ -78,17 +94,17 @@ class RosCommandExecuter():
                         return
 
                     data = buffering['data']
-                    data.append(fd.readline())
+                    data.append({'data': fd.readline(), 'type': 'stdout'})
                     if time.time() - buffering['start_time'] > 0.2:
                         self.sendRosResponse(data=data)
                         buffering['data'] = []
                         buffering['start_time'] = time.time()
-                        
+
                 except Exception as e:
                     print "ERROR stdout_data_available", e
 
             # handle file descriptor with the event loop
-            IOLoop.current().add_handler(
-                self.current_process.stdout, stdout_data_available, IOLoop.current().READ | IOLoop.current().ERROR)
+            IOLoop.current().add_handler(self.current_process.stdout, stdout_data_available,
+                                         IOLoop.current().READ | IOLoop.current().ERROR)
         except Exception as e:
             print "ERROR", e
