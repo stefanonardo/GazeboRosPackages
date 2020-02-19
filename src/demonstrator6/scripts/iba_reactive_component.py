d08d1c3b7df9a68cf7c74d9356abfaddfaedb5ab
Thomas Passer Jensen <thompa@elektro.dtu.dk>
2020-02-19 11:32:26 +0100

    Reactive component with constant trigger pulse width

--- src/demonstrator6/scripts/iba_reactive_component.py
+++ src/demonstrator6/scripts/iba_reactive_component.py
@@ -13,22 +13,34 @@ class IBAReactiveComponent(ExternalModule):
         self.steps = steps
 
     def initialize(self):
-        self.obj_min = 90
-        self.obj_max = 110
-        self.US = None
+        self.obj_min = 80 # could change it such that if it jumps from e.g 115 -> 85 it still gives a pulse..?
+        self.obj_max = 100 # old 90-110
+        self.US = False # None
 
         rospy.Subscriber("/obj_pos", Point, self.obj_pos_callback)
         self.react_pub = rospy.Publisher('/reactive_trigger', Bool, queue_size=10)
+        self.US_steps_left = 0
+        self.had_US = False
 
     def run_step(self):
-        if self.US is not None:
-            trig_msg = Bool()
-            trig_msg.data = self.US
-            self.react_pub.publish(trig_msg)
+        trig_msg = Bool()
+        trig_msg.data = int(self.US)
+        self.react_pub.publish(trig_msg)
+
+        if self.US: # trigger steps countdown
+            self.US_steps_left -= 1
+            if self.US_steps_left == 0:
+                self.US = False
 
     def obj_pos_callback(self, msg):
         obj_pos = msg.x
-        self.US = int( obj_pos > self.obj_min and obj_pos < self.obj_max )
+        if not self.had_US and (obj_pos > self.obj_min and obj_pos < self.obj_max): # If object in zone and we're not already triggering
+            self.US = True
+            self.US_steps_left = 10 # how many steps to trigger for
+            self.had_US = True
+        
+        if obj_pos > self.obj_max: # reset flag to allow detection in zone again
+            self.had_US = False
 
 
 if __name__ == "__main__":
